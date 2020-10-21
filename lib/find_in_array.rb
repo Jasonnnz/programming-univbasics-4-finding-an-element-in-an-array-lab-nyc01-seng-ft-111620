2bceeeec701ea06e22736fda842d6bd5a2bf0a9c
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-21 21:52:43 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,4 +1,9 @@
 def find_element_index(array, value_to_find)
   if array.include?(value_to_find)
-    for i in 1..array.length 
+    for i in 1..array.length
+      if array[i] == value_to_find
+        return i
+  else
+    return NIL
+  end
 end
